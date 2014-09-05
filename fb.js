0d3ebb18c5891a47cfdda595d676d85d1100eb6f
dingshu <ds303077135@gmail.com>
2014-09-05 22:48:27 +0800

    API test

--- fb.js
+++ fb.js
@@ -113,7 +113,7 @@ var deathAnimation = function(){
 	}
 	else {
         ctx.drawImage(scoreBoard, width / 2 - 118, height / 2 - 54);
-        alert(wechat);
+        alert([window.window.WeixinApi , window.WeixinJSBridge]);
         if(wechat) {
             alert("您在 " + ["easy", "normal", "hard"][mode] + " 模式中取得 " + score + " 分，右上角分享成绩到朋友圈吧~");
         }
